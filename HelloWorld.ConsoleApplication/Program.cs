8f7426564c9bda43db672639d79b8ec18785439f
Kenny Pflug <kenny.pflug@live.de>
2015-04-09 14:19:19 +0200

    added example for call by reference, namspaces

--- HelloWorld.ConsoleApplication/Program.cs
+++ HelloWorld.ConsoleApplication/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Timer = System.Timers.Timer;
 
 namespace HelloWorld.ConsoleApplication
 {
@@ -10,16 +8,27 @@ namespace HelloWorld.ConsoleApplication
     {
         static void Main()
         {
-            Person person1 = new Person("Walter", "White");
-            Console.WriteLine(person1.Vorname);
+            var meinePerson = new Person("Walter", "White");
+            VeränderePerson(meinePerson);
+            Console.WriteLine(meinePerson.Nachname);
 
-            person1.Alter = 52;
-            //person1.Alter += 1;
-            person1.Alter++;
+            int integerWert = 42;
+            VerändereInteger(ref integerWert);
+            Console.WriteLine(integerWert);
 
-            Person person2 = new Person(Console.ReadLine());
-            person2.Alter = 27;
+            var timer = new Timer();
 
+            Thread.Sleep(500);
+        }
+
+        private static void VeränderePerson(Person person)
+        {
+            person.Nachname = "Hodor";
+        }
+
+        private static void VerändereInteger(ref int i)
+        {
+            i = 5;
         }
     }
 }
