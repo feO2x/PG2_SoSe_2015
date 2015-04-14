e751ed0a36aa94242fdab1fcb63c7f811a803ed8
Kenny Pflug <kenny.pflug@live.de>
2015-04-14 12:27:55 +0200

    added example for virtual

---
+++ HelloWorld.ConsoleApplication/Gehaltsrechner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.ConsoleApplication
+{
+    public class Gehaltsrechner
+    {
+        public virtual decimal BerechneGehaltserhöhung(Mitarbeiter mitarbeiter)
+        {
+            if (mitarbeiter.Verhältnis == Mitarbeiter.Manager)
+                return mitarbeiter.AktuellesGehalt * 0.5m;
+            if (mitarbeiter.Verhältnis == Mitarbeiter.Teamleiter)
+                return mitarbeiter.AktuellesGehalt * 0.2m;
+            if (mitarbeiter.Verhältnis == Mitarbeiter.Angestellte)
+                return mitarbeiter.AktuellesGehalt * 0.05m;
+
+            return -(mitarbeiter.AktuellesGehalt * 0.2m);
+        }
+    }
+
+    public class GehaltrechnerMitTracking : Gehaltsrechner
+    {
+        private int _anzahlAufrufe = 0;
+
+        public override decimal BerechneGehaltserhöhung(Mitarbeiter mitarbeiter)
+        {
+            _anzahlAufrufe++;
+            return base.BerechneGehaltserhöhung(mitarbeiter);
+        }
+    }
+
+    public class Mitarbeiter
+    {
+        public const string Manager = "Manager";
+        public const string Teamleiter = "Teamleiter";
+        public const string Angestellte = "Angestellte";
+
+        public string Name { get; set; }
+        public string Verhältnis { get; set; }
+        public decimal AktuellesGehalt { get; set; }
+    }
+}
--- HelloWorld.ConsoleApplication/Program.cs
+++ HelloWorld.ConsoleApplication/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld.ConsoleApplication
             get { return _zähler; }
         }
 
-        public void ErhöheZähler()
+        public virtual void ErhöheZähler()
         {
             _zähler++;
         }
@@ -19,48 +19,28 @@ namespace HelloWorld.ConsoleApplication
 
     public class B : A // Subklasse 
     {
-        public void ErhöheZählerUmDrei()
+        public override void ErhöheZähler()
         {
             _zähler += 3;
         }
     }
 
-    public class C : A
-    {
-        public void ErhöheZählerUmFünf()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                ErhöheZähler();
-            }
-        }
-    }
 
     class Program
     {
         static void Main()
         {
-            B objekt1 = new B();
+            A objekt1 = new A();
             objekt1.ErhöheZähler();
-            objekt1.ErhöheZählerUmDrei();
             Console.WriteLine(objekt1.Zähler);
 
-            A objekt2 = new A();
+            B objekt2 = new B();
             objekt2.ErhöheZähler();
             Console.WriteLine(objekt2.Zähler);
 
-            A objekt3 = new B(); // Ist-Eine-Beziehung
+            A objekt3 = new B();
             objekt3.ErhöheZähler();
-            // objekt3.ErhöheZählerUmDrei(); Geht nicht, da Variablentyp A ist
             Console.WriteLine(objekt3.Zähler);
-
-            B objekt4 = (B)objekt3;
-            objekt4.ErhöheZählerUmDrei();
-            A objekt5 = new C();
-            // B objekt6 = (B)objekt5; Exception: falscher Downcast
-
-            B objekt6 = objekt5 as B;
-            B objekt7 = objekt3 as B;
 
         }
     }
