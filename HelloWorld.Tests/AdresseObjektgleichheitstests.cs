65a18a9a4b9474ca27a1ed4c4aeb734f530f847e
Kenny Pflug <kenny.pflug@live.de>
2015-04-28 11:54:37 +0200

    added more unit tests as example

---
+++ HelloWorld.Tests/AdresseObjektgleichheitstests.cs
@@ -0,0 +1,62 @@
+﻿using HelloWorld.ConsoleApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class AdresseObjektgleichheitstests
+    {
+        [TestMethod]
+        public void ZweiAdressenMitIdentischenWertenErzeugenDenselbenHashCode()
+        {
+            // Arrange
+            var ersteAdresse = new Adresse("Universitätsstr.", "93053", "Regensburg");
+            var zweiteAdresse = new Adresse("Universitätsstr.", "93053", "Regensburg");
+
+            // Act
+            var ersterHashCode = ersteAdresse.GetHashCode();
+            var zweiteHashCode = zweiteAdresse.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(ersterHashCode, zweiteHashCode);
+        }
+
+        [TestMethod]
+        public void EqualsGibtTrueZurückBeiAnderemObjektMitIdentischenWerten()
+        {
+            // Arrange
+            var ersteAdresse = new Adresse("Universitätsstr.", "93053", "Regensburg");
+            var zweiteAdresse = new Adresse("Universitätsstr.", "93053", "Regensburg");
+
+            // Act und Assert in einem Schritt
+            Assert.IsTrue(ersteAdresse.Equals(zweiteAdresse));
+        }
+
+        [TestMethod]
+        public void EqualsGibtFalseZurückBeiUnterschiedlichenObjektenMitUnterschiedlichenWerten()
+        {
+            // Arrange
+            var ersteAdresse = new Adresse("Universitätsstr.", "93053", "Regensburg");
+            var zweiteAdresse = new Adresse("Beispielstr.", "93047", "Regensburg");
+
+            Assert.IsFalse(ersteAdresse.Equals(zweiteAdresse));
+        }
+
+        [TestMethod]
+        public void EqualsGibtFalseZurückWennNullÜbergebenWird()
+        {
+            var adresse = new Adresse("Foo", "Bar", "Baz");
+
+            Assert.IsFalse(adresse.Equals(null));
+        }
+
+        [TestMethod]
+        public void EqualsGibtFalseZurückWennObjektEinerAnderenKlasseÜbergebenWird()
+        {
+            var adresse = new Adresse("Foo", "Bar", "Baz");
+            var anderesObjekt = new Farbe(128, 128, 128);
+
+            Assert.IsFalse(adresse.Equals(anderesObjekt));
+        }
+    }
+}
