c050450d464b1a9ab313757d099dd7547ae12f62
Kenny Pflug <kenny.pflug@live.de>
2015-05-19 13:16:11 +0200

    added Master-Detail example for WPF

---
+++ HelloWorld.WpfClient/Kontakt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld.WpfClient
+{
+    public class Kontakt
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public DateTime? Geburtsdatum { get; set; }
+
+        public string VollerName
+        { get { return Vorname + " " + Nachname; } }
+
+        public override string ToString()
+        {
+            return VollerName;
+        }
+    }
+}
--- HelloWorld.WpfClient/MainWindow.xaml.cs
+++ HelloWorld.WpfClient/MainWindow.xaml.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HelloWorld.WpfClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public IList<Kontakt> Kontakte
+        {
+            get { return MasterListBox.ItemsSource as IList<Kontakt>; }
+            set { MasterListBox.ItemsSource = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Kontakte = new List<Kontakt>
+            {
+                new Kontakt{Vorname = "Walter", Nachname = "White", Geburtsdatum = new DateTime(1958, 5, 20), Email = "walter@white.com"},
+                new Kontakt{Vorname = "Jesse", Nachname = "Pinkman", Geburtsdatum = new DateTime(1987, 4, 25), Telefon = "312124 12332"}
+            };
         }
 
-        private void WennButtonGedrücktWird(object sender, RoutedEventArgs e)
+        private void WennKontaktAusgewähltWird(object sender, SelectionChangedEventArgs e)
         {
-            MeinTextBlock.Text = "Hello WPF";
+            var selektierterKontakt = MasterListBox.SelectedItem as Kontakt;
+
+            NameTextBlock.Text = selektierterKontakt.VollerName;
+            GeburtsdatumTextBlock.Text = "Geburtsdatum: ";
+            if (selektierterKontakt.Geburtsdatum.HasValue)
+                GeburtsdatumTextBlock.Text += selektierterKontakt.Geburtsdatum.Value.ToShortDateString();
+            MailTextBlock.Text = "Mail: " + selektierterKontakt.Email;
+            TelefonTextBlock.Text = "Telefon: " + selektierterKontakt.Telefon;
         }
+
     }
 }
