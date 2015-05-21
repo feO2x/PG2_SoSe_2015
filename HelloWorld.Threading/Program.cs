0b79ac431f433175b146b4265c4bccc39bd46a16
Kenny Pflug <kenny.pflug@live.de>
2015-05-21 14:42:23 +0200

    added example for new Thread

---
+++ HelloWorld.Threading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld.Threading
+{
+    class Program
+    {
+        static void Main()
+        {
+            var jetzigerThread = Thread.CurrentThread;
+            Console.WriteLine("Start auf Thread " + jetzigerThread.ManagedThreadId);
+
+            var neuerThread = new Thread(ZähleHoch)
+                              {
+                                  Name = "Neuer Thread",
+                                  //IsBackground = true
+                              };
+            neuerThread.Start();
+
+            Console.ReadLine();
+
+        }
+
+        private static void ZähleHoch()
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Durchlauf {0} auf Thread {1}", i + 1, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
