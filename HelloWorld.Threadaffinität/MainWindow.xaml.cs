ce4298e21591a3a72f10451d3cbe2def5073f80f
Kenny Pflug <kenny.pflug@live.de>
2015-05-21 14:31:49 +0200

    added example for UI freezing

--- HelloWorld.Threadaffinität/MainWindow.xaml.cs
+++ HelloWorld.Threadaffinität/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace HelloWorld.Threadaffinität
@@ -12,7 +14,36 @@ namespace HelloWorld.Threadaffinität
 
         private void WennButtonGedrücktWird(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Button.IsEnabled = false;
+            ProgressBar.IsIndeterminate = true;
+
+            var untereGrenze = Convert.ToInt32(VonTextBox.Text);
+            var obereGrenze = Convert.ToInt32(BisTextBox.Text);
+
+            var divisoren = Enumerable.Range(untereGrenze, obereGrenze - untereGrenze + 1)
+                                      .ToList();
+
+            for (long i = 1; i <= int.MaxValue; i++)
+            {
+                if (ÜberprüfeTeilbarkeit(i, divisoren))
+                {
+                    ErgebnisTextBlock.Text = "Die gesuchte Zahl ist " + i.ToString("N0");
+                    break;
+                }
+            }
+
+            Button.IsEnabled = true;
+            ProgressBar.IsIndeterminate = false;
+        }
+
+        private bool ÜberprüfeTeilbarkeit(long zahl, List<int> divisoren)
+        {
+            foreach (var divisor in divisoren)
+            {
+                if (zahl % divisor != 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
