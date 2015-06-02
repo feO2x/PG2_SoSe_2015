b2421efcc9d208a22801f0c4b58de1b337e22ba8
Kenny Pflug <kenny.pflug@live.de>
2015-06-02 11:37:24 +0200

    removed unnecessary using statements

--- HelloWorld.ConsoleApplication/Program.cs
+++ HelloWorld.ConsoleApplication/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Xml;
 
 namespace HelloWorld.ConsoleApplication
 {
