5c9ee275bb4855c3213b7b5fefa9551cc8bafc02
Kenny Pflug <kenny.pflug@live.de>
2015-06-02 14:16:35 +0200

    added example for Liskov Substitution Principle

---
+++ HelloWorld.Tests/Rechteck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Tests
+{
+    public class Rechteck
+    {
+        private int _höhe;
+        private int _breite;
+
+        public virtual int Höhe
+        {
+            get { return _höhe; }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                _höhe = value;
+            }
+        }
+
+        public virtual int Breite
+        {
+            get { return _breite; }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                _breite = value;
+            }
+        }
+
+        public int BerechneFläche()
+        {
+            return _höhe * _breite;
+        }
+    }
+
+    public class Quadrat : Rechteck
+    {
+        public override int Breite
+        {
+            get { return base.Breite; }
+            set
+            {
+                base.Breite = value;
+                base.Höhe = value;
+            }
+        }
+
+        public override int Höhe
+        {
+            get { return base.Höhe; }
+            set
+            {
+                base.Höhe = value;
+                base.Breite = value;
+            }
+        }
+    }
+}
---
+++ HelloWorld.Tests/RechteckTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class RechteckTests
+    {
+        [TestMethod]
+        public void BerechneFlächeFunktioniertFürRechteck()
+        {
+            var rechteck = new Rechteck();
+            ÜberprüfeFläche(rechteck);
+        }
+
+        [TestMethod]
+        public void BerechneFlächeFunktioniertFürQuadrat()
+        {
+            var quadrat = new Quadrat();
+            ÜberprüfeFläche(quadrat);
+        }
+
+        private static void ÜberprüfeFläche(Rechteck rechteck)
+        {
+            rechteck.Höhe = 4;
+            rechteck.Breite = 5;
+            Assert.AreEqual(20, rechteck.BerechneFläche());
+        }
+    }
+}
