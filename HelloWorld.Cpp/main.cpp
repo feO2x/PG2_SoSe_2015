9355ff72c54dba190870d7f97e98edd3a2dbe1e4
Kenny Pflug <kenny.pflug@live.de>
2015-06-18 14:45:24 +0200

    added example for inheritance and is-a-relationship

--- HelloWorld.Cpp/main.cpp
+++ HelloWorld.Cpp/main.cpp
@@ -6,16 +6,50 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-void VerdoppeleRechteckgröße(Rechteck& rechteck)
+class A
 {
-	rechteck._höhe *= 2;
-	rechteck._breite *= 2;
-}
+private:
+	int _counterA = 0;
+
+public:
+	void Increment()
+	{
+		_counterA++;
+	}
+
+	int GetCounter()
+	{
+		return _counterA;
+	}
+};
+
+class B : public A
+{
+	int _counterB = 1;
+public:
+	void IncrementBy3()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			Increment();
+		}
+	}
+};
 
 void main()
 {
-	Rechteck* rechteck = new Rechteck(5);
-	VerdoppeleRechteckgröße(*rechteck);
+	B objekt1;
+	
+	objekt1.Increment();
+	objekt1.IncrementBy3();
 
-	delete rechteck;
+	A* pointerToObjekt1 = &objekt1; // Ist-Eine-Beziehung
+	pointerToObjekt1->Increment();
+
+	A& referenceToObjekt1 = objekt1; // Ist-Eine-Beziehung
+	referenceToObjekt1.Increment();
+
+	// Ist-Eine-Beziehung ohne Pointer oder Referenzen
+	A objekt2 = objekt1; // Slicing - das ist meistens nicht gewünscht
+	objekt2.GetCounter();
 }
