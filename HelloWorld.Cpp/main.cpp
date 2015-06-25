070f8e35a9d1384c6d732219c06a348570030da2
Kenny Pflug <kenny.pflug@live.de>
2015-06-25 12:58:33 +0200

    added example for using vectors

--- HelloWorld.Cpp/main.cpp
+++ HelloWorld.Cpp/main.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
-#include "Person.h"
-#include "Rechteck.h"
+#include <vector>
 
-using std::cout;
-using std::endl;
-using std::cin;
-
-class A
-{
-private:
-	int _counterA = 0;
-
-public:
-	void Increment()
-	{
-		_counterA++;
-	}
-
-	int GetCounter()
-	{
-		return _counterA;
-	}
-};
-
-class B : public A
-{
-	int _counterB = 1;
-public:
-	void IncrementBy3()
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			Increment();
-		}
-	}
-};
+using namespace std;
 
 void main()
 {
-	B objekt1;
-	
-	objekt1.Increment();
-	objekt1.IncrementBy3();
+	// Vector on the stack
+	vector<int> integers;
 
-	A* pointerToObjekt1 = &objekt1; // Ist-Eine-Beziehung
-	pointerToObjekt1->Increment();
+	// add elements at the end of the collection
+	integers.push_back(2);
+	integers.push_back(4);
+	integers.push_back(7);
 
-	A& referenceToObjekt1 = objekt1; // Ist-Eine-Beziehung
-	referenceToObjekt1.Increment();
+	// Random access with at or index notation
+	cout << "Element at position 1: " << integers.at(1) << endl;
+	cout << "Element at position 2: " << integers[2] << endl << endl;
 
-	// Ist-Eine-Beziehung ohne Pointer oder Referenzen
-	A objekt2 = objekt1; // Slicing - das ist meistens nicht gewünscht
-	objekt2.GetCounter();
+	// Run through the whole collection with random access
+	cout << "All elements:" << endl;
+	for (int i = 0; i < integers.size(); i++)
+	{
+		cout << "  " << integers[i] << endl;
+	}
+	cout << endl;
+
+	// Run through the whole collection using iterators
+	cout << "Again all elements: " << endl;
+	for (auto startIterator = integers.begin(); startIterator < integers.end(); ++startIterator)
+	{
+		cout << *startIterator << endl;
+	}
+	cout << endl;
+
+	// Inserts can only be done with iterators
+	integers.insert(integers.begin() + 2, 8);
+
+	cout << "After insertion:" << endl;
+	for (auto number : integers)
+	{
+		cout << number << endl;
+	}
+	cout << endl;
+
+	// Removal can be done only via index
+	integers.erase(integers.begin());
+
+	// Removal by element must be performed with a previous search (use std::find for that)
+	auto removeIterator = find(integers.begin(), integers.end(), 7);
+	integers.erase(removeIterator);
+
+	cout << "After removal:" << endl;
+	for (auto number : integers)
+	{
+		cout << number << endl;
+	}
+	cout << endl;
 }
